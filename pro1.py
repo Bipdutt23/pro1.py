77f5572e066748032d16f22ca365e041f5e8cb12
Bipdutt23 <115216437+Bipdutt23@users.noreply.github.com>
2022-10-07 13:52:25 +0530

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a number"))
+b=int(input("Enter another number"))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("the sum is",c)
+print("the difference is",d)
+print("the product is",e)
+print("the quotient is",f)      
